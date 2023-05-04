b3637917a44334d8da9e35996e5525efb5b5ba7b
Michael Christensen <michaeldev@musicmichaelc.com>
2023-05-04 13:19:48 +0200

    Add event listener that deletes todos
    Deletes todo items when clicking on trash icon. Following lesson 80

--- js/app.js
+++ js/app.js
@@ -23,3 +23,11 @@ addForm.addEventListener('submit', e => {
     addForm.reset();
   }
 });
+
+
+// Delete todos:
+list.addEventListener('click', e => {
+  if(e.target.classList.contains('delete')){
+    e.target.parentElement.remove();
+  }
+});
